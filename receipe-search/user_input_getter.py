5384683e86ac3d0a12d727dfe13356cb3d5944f6
cenkgurbet <cenk.gurbet@deepnetwork.com>
2024-01-09 18:43:30 +0300

    Add Count - 4

--- receipe-search/user_input_getter.py
+++ receipe-search/user_input_getter.py
@@ -2,7 +2,6 @@ import json
 
 import azure.cognitiveservices.speech as speechsdk
 from openai_ask import openai_ask_meal_init
-from openai_ask import openai_ask_meal_check
 from openai_ask import openai_ask_language
 from openai_ask import openai_ask_general_check
 from openai_ask import openai_ask_count_init
@@ -12,11 +11,13 @@ from speaker_ask import ask_to_user
 stt_key='05e0d71cfca242cbaa7117d138f394ff'
 stt_location='germanywestcentral'
 
+
 def input_handler(user_inp, language_inp, meal_or_count):
     # Getting language of user prompt
     language = language_inp
     if language_inp == "NOT_DEFINED":
         language = openai_ask_language(user_inp)
+    print("Used Language is: " + language)
 
     # Scraping meal/count value
     if meal_or_count == "meal":
@@ -30,12 +31,14 @@ def input_handler(user_inp, language_inp, meal_or_count):
             openai_answer_general_for_meal_json = json.loads(openai_answer_for_meal_general.choices[0].message.content)
             print("Result of the double check: " + openai_answer_general_for_meal_json['is_that_type'])
     elif meal_or_count == "count":
-        openai_answer_for_count_init = openai_ask_meal_init(user_inp, language)
+        openai_answer_for_count_init = openai_ask_count_init(user_inp, language)
         openai_answer_for_count_init_json = json.loads(openai_answer_for_count_init.choices[0].message.content)
         print("Input value: " + user_inp)
-        print("If given value is meal/food or not: " + str(openai_answer_for_count_init_json['is_count']))
+        print("If given value is count/number or not: " + str(openai_answer_for_count_init_json['is_count']))
 
-        if openai_answer_for_count_init_json['is_meal']:
+        if openai_answer_for_count_init_json['is_count']:
+            print(openai_answer_for_count_init_json['number_of_people'])
+            print(language)
             openai_answer_for_count_general = openai_ask_general_check(openai_answer_for_count_init_json['number_of_people'], "integer", language)
             openai_answer_general_for_count_json = json.loads(openai_answer_for_count_general.choices[0].message.content)
             print("Result of the double check: " + openai_answer_general_for_count_json['is_that_type'])
@@ -44,7 +47,6 @@ def input_handler(user_inp, language_inp, meal_or_count):
 
 
 def recognize_from_microphone():
-    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
     speech_config = speechsdk.SpeechConfig(stt_key, stt_location)
     speech_config.set_property(speechsdk.PropertyId.SpeechServiceConnection_EndSilenceTimeoutMs, "10")
     auto_detect_source_language_config = speechsdk.languageconfig.AutoDetectSourceLanguageConfig(languages=["en-UK", "de-DE", "tr-TR"])
