b21863b9aa15d51a52d8586e6989ec642f2dd00e
cenkgurbet <cenk.gurbet@deepnetwork.com>
2024-01-09 17:11:45 +0300

    Add Count - 2

--- receipe-search/user_input_getter.py
+++ receipe-search/user_input_getter.py
@@ -26,27 +26,42 @@ def recognize_from_microphone():
 
     while True:
         print("Tell your request: ")
-        user_input = speech_recognizer.recognize_once()
-        auto_detect_source_language_result = speechsdk.AutoDetectSourceLanguageResult(user_input)
-        detected_language = auto_detect_source_language_result.language
+        user_input_for_meal = speech_recognizer.recognize_once()
+        auto_detect_source_language_result_for_meal = speechsdk.AutoDetectSourceLanguageResult(user_input_for_meal)
+        detected_language_for_meal = auto_detect_source_language_result_for_meal.language
 
-        if user_input.reason == speechsdk.ResultReason.RecognizedSpeech:
+        # Get Meal ---
+        if user_input_for_meal.reason == speechsdk.ResultReason.RecognizedSpeech:
+            print("Language: " + detected_language_for_meal)
+            openai_answer_for_meal_init = openai_ask_meal_init(user_input_for_meal, detected_language_for_meal)
+            openai_answer_for_meal_init_json = json.loads(openai_answer_for_meal_init.choices[0].message.content)
+            print("Input value: " + user_input_for_meal)
+            print("If given value is meal/food or not: " + str(openai_answer_for_meal_init_json['is_meal']))
 
-            openai_answer_init = openai_ask_meal_init(user_input.text)
-            if openai_answer_init == "not_stated" or not ask_to_user(openai_answer_init, detected_language, 2):
-                continue
+            if openai_answer_for_meal_init_json['is_meal']:
+                openai_answer_for_meal_general = openai_ask_general_check(openai_answer_for_meal_init_json['meal_name'], "dish name", user_input_for_meal_language)
+                openai_answer_general_for_meal_json = json.loads(openai_answer_for_meal_general.choices[0].message.content)
+                print("Result of the double check: " + openai_answer_general_for_meal_json['is_that_type'])
+        else:
+            print("Speeh error.")
+            print("Did you set the speech resource key and region values?")
 
-            openai_answer_check = openai_ask_meal_check(openai_answer_init)
-            print(openai_answer_check)
-            print("..............................................................")
-        elif user_input.reason == speechsdk.ResultReason.NoMatch:
-            print("No speech could be recognized: {}".format(user_input.no_match_details))
-        elif user_input.reason == speechsdk.ResultReason.Canceled:
-            cancellation_details = user_input.cancellation_details
-            print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-            if cancellation_details.reason == speechsdk.CancellationReason.Error:
-                print("Error details: {}".format(cancellation_details.error_details))
-                print("Did you set the speech resource key and region values?")
+        # Get Count ---
+        user_input_for_count = speech_recognizer.recognize_once()
+        auto_detect_source_language_result_for_meal = speechsdk.AutoDetectSourceLanguageResult(user_input_for_meal)
+        detected_language_for_meal = auto_detect_source_language_result_for_meal.language
+
+        if user_input_for_count.reason == speechsdk.ResultReason.RecognizedSpeech:
+            openai_answer_for_count_init = openai_ask_count_init(user_input_for_count, detected_language_for_meal)
+            openai_answer_for_count_init_json = json.loads(openai_answer_for_count_init.choices[0].message.content)
+            print("Input value: " + user_input_for_count)
+            print("If given value is valid count or not: " + str(openai_answer_for_count_init_json['is_count']))
+
+            if openai_answer_for_count_init_json['is_count']:
+                openai_answer_general = openai_ask_general_check(str(openai_answer_for_count_init_json['number_of_people']), "integer", user_input_for_count_language)
+                openai_answer_general_json = json.loads(openai_answer_general.choices[0].message.content)
+                print("Result of the double check: " + openai_answer_general_json['is_that_type'])
+        print("..............................................................")
 
 
 def recognize_from_text():
