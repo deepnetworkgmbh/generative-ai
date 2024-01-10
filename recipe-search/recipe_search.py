00b39cc6dca646237a94cf9b8192505fc4f34008
Emrah Kaya <emrahkaya@gmail.com>
2024-01-10 16:01:59 +0300

    Refactor, fix

--- recipe-search/recipe_search.py
+++ recipe-search/recipe_search.py
@@ -1,6 +1,7 @@
 from user_input_getter import recognize_from_microphone
 from user_input_getter import recognize_from_text
 
+
 def get_user_request():
     user_input = input('''Select input type:
 1) Text
@@ -14,6 +15,8 @@ def get_user_request():
     else:
         print("Error Input")
 
+
 output_data = get_user_request()
-print("DISH: " + output_data['dish_name'])
-print("SERVING COUNT: " + str(output_data['serving_count']))
+if output_data:
+    print("DISH: " + output_data['dish_name'])
+    print("SERVING COUNT: " + str(output_data['serving_count']))
