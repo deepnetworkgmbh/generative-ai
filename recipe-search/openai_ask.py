be1b7d7de5f1058f6a5aa17893d7a983cd3fe29b
cenkgurbet <cenk.gurbet@deepnetwork.com>
2024-01-10 13:07:44 +0300

    Add Count - 5

--- recipe-search/openai_ask.py
+++ recipe-search/openai_ask.py
@@ -8,7 +8,7 @@ client = AzureOpenAI(
 )
 
 
-def openai_ask_meal_init(user_request, language):
+def openai_ask_dish_init(user_request, language):
     # 1. Ask ---
     # parameters_schema = {
     #     "type": "object",
@@ -27,15 +27,15 @@ def openai_ask_meal_init(user_request, language):
     #     "parameters": parameters_schema,
     # }
 
-    messages_init = [{"role": "system", "content": "You are an assistant that gets food name from the statement."
+    messages_init = [{"role": "system", "content": "You are an assistant that gets dish name from the statement."
                                                    "Return json object which consists of 2 fields."
-                                                   "These fields are 'is_meal' and 'meal_name'"
+                                                   "These fields are 'is_dish' and 'dish_name'"
                                                    "Do not give other information in json."
                                                    "Do not give instructions or ingredients in json object."
-                                                   "If you can find meal name, set 'is_meal' true, and 'meal_name' name of the meal"
-                                                   "If you can not find meal name, set 'is_meal' false, and 'meal_name' not_stated"
-                                                   "Meal name can be just a product such as apple, orange etc. If so, set 'is_meal' true, and 'meal_name' name of the product (apple etc.)"
-                                                   f"Language of the meal is {language}, so please consider that language during meal/food search."
+                                                   "If you can find meal name, set 'is_dish' true, and 'dish_name' name of the dish"
+                                                   "If you can not find dish name, set 'is_dish' false, and 'dish_name' not_stated"
+                                                   "Dish name can not be just a product such as apple, orange etc. If so, set 'is_dish' false, and 'dish_name' not_stated"
+                                                   f"Language of the dish is {language}, so please consider that language during dish search."
                       }, {"role": "user", "content": user_request}]
 
     response_init = client.chat.completions.create(
@@ -87,8 +87,8 @@ def openai_ask_general_check(input, type, language): # input, type --> if 'input
     # 2. Ask - Double Check ---
     messages_check = [{"role": "system", "content": f"You are an assistant that check if the given input is actually a {type} or not in {language}."
                                                     "Return json object which consists of 1 field called 'is_that_type'."
-                                                    f"Set 'is_that_type' field 'Yes' if it is {type} in {language}."
-                                                    f"Set 'is_that_type' field 'No' if it is not {type} in {language}."                                                    
+                                                    f"Set 'is_that_type' field 'True' in string format if it is {type} in {language}."
+                                                    f"Set 'is_that_type' field 'False' in string format if it is not {type} in {language}."                                                    
                                                     f"Language of the input is {language}, so please consider that language during type check."
                        }, {"role": "user", "content": str(input)}]
 
