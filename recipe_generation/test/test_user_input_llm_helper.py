c19e217d6d38c60cbf06357fe28a2c7a7b3db17f
Emrah Kaya <emrahkaya@gmail.com>
2024-01-18 13:57:14 +0300

    Rename folder

--- recipe_generation/test/test_user_input_llm_helper.py
+++ recipe_generation/test/test_user_input_llm_helper.py
@@ -2,127 +2,150 @@ import json
 import os
 import time
 import unittest
+from pathlib import Path
+
 from openai import AzureOpenAI
+
 from recipe_generator.user_input_llm_helper import UserInputLlmHelper
 import test_helpers
 
 
 class TestUserInputLlmHelper(unittest.TestCase):
+    dish_name_in_sentences_english = []
+    dish_name_in_sentences_german = []
+    dish_name_in_sentences_turkish = []
+    number_in_sentences_english = []
+    number_in_sentences_german = []
+    number_in_sentences_turkish = []
+    dish_name_and_number_in_sentences_mixed_lang = []
+    dish_name_or_number_in_sentences_mixed_lang = []
+    test_metrics = []
 
-    def __init__(self, *args, **kwargs):
-        super(TestUserInputLlmHelper, self).__init__(*args, **kwargs)
-        # Custom fields
-        self.azure_openai_model_name = os.getenv('AZURE_OPENAI_GPT_DEPLOYMENT')
-        self.azure_openai_client = AzureOpenAI(api_version="2023-09-01-preview")
-        self.user_input_llm_helper = UserInputLlmHelper(self.azure_openai_client, self.azure_openai_model_name)
-        self.dish_name_in_sentences_english = []
-        self.dish_name_in_sentences_german = []
-        self.dish_name_in_sentences_turkish = []
-        self.number_in_sentences_english = []
-        self.number_in_sentences_german = []
-        self.number_in_sentences_turkish = []
-        self.dish_name_and_number_in_sentences_mixed_lang = []
-        self.dish_name_or_number_in_sentences_mixed_lang = []
+    @classmethod
+    def setUpClass(cls):
+        azure_openai_model_name = os.getenv('AZURE_OPENAI_GPT_DEPLOYMENT')
+        azure_openai_client = AzureOpenAI(api_version="2023-09-01-preview")
+        cls.user_input_llm_helper = UserInputLlmHelper(azure_openai_client, azure_openai_model_name)
 
         with open('test_data/dish_name_in_sentences_english.txt', 'r') as file:
             for line in file:
-                self.dish_name_in_sentences_english.append(line.strip())
+                cls.dish_name_in_sentences_english.append(line.strip())
 
         with open('test_data/dish_name_in_sentences_german.txt', 'r') as file:
             for line in file:
-                self.dish_name_in_sentences_german.append(line.strip())
+                cls.dish_name_in_sentences_german.append(line.strip())
 
         with open('test_data/dish_name_in_sentences_turkish.txt', 'r') as file:
             for line in file:
-                self.dish_name_in_sentences_turkish.append(line.strip())
+                cls.dish_name_in_sentences_turkish.append(line.strip())
 
         with open('test_data/number_in_sentences_english.txt', 'r') as file:
             for line in file:
-                self.number_in_sentences_english.append(line.strip())
+                cls.number_in_sentences_english.append(line.strip())
 
         with open('test_data/number_in_sentences_german.txt', 'r') as file:
             for line in file:
-                self.number_in_sentences_german.append(line.strip())
+                cls.number_in_sentences_german.append(line.strip())
 
         with open('test_data/number_in_sentences_turkish.txt', 'r') as file:
             for line in file:
-                self.number_in_sentences_turkish.append(line.strip())
+                cls.number_in_sentences_turkish.append(line.strip())
 
         with open('test_data/dish_name_and_number_in_sentences_mixed_lang.txt', 'r') as file:
             for line in file:
                 sentence = line.split(" - ")
-                self.dish_name_and_number_in_sentences_mixed_lang.append([sentence[0], sentence[1]])
+                cls.dish_name_and_number_in_sentences_mixed_lang.append([sentence[0], sentence[1]])
 
         with open('test_data/dish_name_or_number_in_sentences_mixed_lang.txt', 'r') as file:
             for line in file:
                 sentence = line.split(" - ")
-                self.dish_name_or_number_in_sentences_mixed_lang.append([sentence[0], sentence[1], sentence[2]])
+                cls.dish_name_or_number_in_sentences_mixed_lang.append([sentence[0], sentence[1], sentence[2]])
+
+    @classmethod
+    def tearDownClass(cls):
+        with open(f'{Path(__file__).stem}_results.json', 'w') as file:
+            json.dump(cls.test_metrics, file, indent=2)
 
     def test_clean_dish_name_english(self):
         results, total_time = run_clean_dish_name(self.user_input_llm_helper, self.dish_name_in_sentences_english, "English")
-        test_helpers.calculate_and_print_metrics(
+        self.test_metrics.append(
+            test_helpers.calculate_metrics(
             "Clean Dish Name: dish_name_in_sentences_english:",
             results,
             total_time
+            )
         )
 
     def test_clean_dish_name_german(self):
         results, total_time = run_clean_dish_name(self.user_input_llm_helper, self.dish_name_in_sentences_german, "German")
-        test_helpers.calculate_and_print_metrics(
-            "Clean Dish Name: dish_name_in_sentences_german:",
-            results,
-            total_time
+        self.test_metrics.append(
+            test_helpers.calculate_metrics(
+                "Clean Dish Name: dish_name_in_sentences_german:",
+                results,
+                total_time
+            )
         )
 
     def test_clean_dish_name_turkish(self):
         results, total_time = run_clean_dish_name(self.user_input_llm_helper, self.dish_name_in_sentences_turkish, "Turkish")
         end_time = time.time()
-        test_helpers.calculate_and_print_metrics(
-            "Clean Dish Name: dish_name_in_sentences_turkish:",
-            results,
-            total_time
+        self.test_metrics.append(
+            test_helpers.calculate_metrics(
+                "Clean Dish Name: dish_name_in_sentences_turkish:",
+                results,
+                total_time
+            )
         )
 
     def test_clean_servings_english(self):
         results, total_time = run_clean_servings(self.user_input_llm_helper, self.number_in_sentences_english, "English")
-        test_helpers.calculate_and_print_metrics(
-            "Clean Servings: number_in_sentences_english:",
-            results,
-            total_time
+        self.test_metrics.append(
+            test_helpers.calculate_metrics(
+                "Clean Servings: number_in_sentences_english:",
+                results,
+                total_time
+            )
         )
 
     def test_clean_servings_german(self):
         results, total_time = run_clean_servings(self.user_input_llm_helper, self.dish_name_in_sentences_german, "German")
-        test_helpers.calculate_and_print_metrics(
-            "Clean Servings: dish_name_in_sentences_german:",
-            results,
-            total_time
+        self.test_metrics.append(
+            test_helpers.calculate_metrics(
+                "Clean Servings: dish_name_in_sentences_german:",
+                results,
+                total_time
+            )
         )
 
     def test_clean_servings_turkish(self):
         results, total_time = run_clean_servings(self.user_input_llm_helper, self.number_in_sentences_turkish, "Turkish")
-        test_helpers.calculate_and_print_metrics(
-            "Clean Servings: number_in_sentences_turkish:",
-            results,
-            total_time
+        self.test_metrics.append(
+            test_helpers.calculate_metrics(
+                "Clean Servings: number_in_sentences_turkish:",
+                results,
+                total_time
+            )
         )
 
     def test_determine_language_mixed_lang(self):
         results, total_time = run_determine_language(self.user_input_llm_helper, self.dish_name_and_number_in_sentences_mixed_lang)
-        test_helpers.calculate_and_print_metrics(
-            "Determine Language: dish_name_and_number_in_sentences_mixed_lang:",
-            results,
-            total_time
+        self.test_metrics.append(
+            test_helpers.calculate_metrics(
+                "Determine Language: dish_name_and_number_in_sentences_mixed_lang:",
+                results,
+                total_time
+            )
         )
 
     def test_does_input_type_match(self):
         results, total_time = run_check_input_type_match(self.user_input_llm_helper, self.dish_name_or_number_in_sentences_mixed_lang)
-        test_helpers.calculate_and_print_metrics(
-            "Does Input Type Match: dish_name_or_number_in_sentences_mixed_lang:",
-            results,
-            total_time
+        self.test_metrics.append(
+            test_helpers.calculate_metrics(
+                "Does Input Type Match: dish_name_or_number_in_sentences_mixed_lang:",
+                results,
+                total_time
+            )
         )
-
 
 def run_clean_dish_name(user_input_llm_helper, data, language):
     start_time = time.time()
